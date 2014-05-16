81e53070ec665477ad881f6b097229107364b045
Ben Kennedy <bgkennedy@gmail.com>
2014-05-16 16:26:10 +0100

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheerupApp::Application.config.secret_token = '271fdddc6c71656410d3cf0359f33277938247f13abc260ccbd1ee90317cfed8c12a0da11a93b0ebd4132be068cc2b2aca9880d7d32d855bc5348bfdde407f82'
