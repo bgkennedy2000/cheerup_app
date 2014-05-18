aa91f615c169f733ed120731b475336f5906ca1d
NoorBL <noorbinladin@gmail.com>
2014-05-18 18:43:45 +0100

    working on cheerups controller

--- app/controllers/cheerups_controller.rb
+++ app/controllers/cheerups_controller.rb
@@ -7,8 +7,6 @@ class CheerupsController < ApplicationController
   end
 
 
-
-
 def create
     @cheerup = Cheerup.new(params[:cheerup])
     @cheerup.user = current_user
@@ -27,7 +25,46 @@ def create
 
 
 
+  def show
+    @cheerup = Cheerup.find(params[:id])
 
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @cheerup }
+    end
+  end
+
+  
+ 
+  def edit
+    @cheerup = Cheerup.find(params[:id])
+  end
+
+ 
+  def update
+    @cheerup = Cheerup.find(params[:id])
+
+    respond_to do |format|
+      if @cheerup.update_attributes(params[:cheerup])
+        format.html { redirect_to @cheerup, notice: 'Cheerup was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @cheerup.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  
+  def destroy
+    @cheerup = Cheeruo.find(params[:id])
+    @cheerup.destroy
+
+    respond_to do |format|
+      format.html { redirect_to cheerups_url }
+      format.json { head :no_content }
+    end
+  end
 
 
 
