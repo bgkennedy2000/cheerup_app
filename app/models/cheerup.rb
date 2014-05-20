0c49b8682cb613f715da09c898904adb2db84802
NoorBL <noorbinladin@gmail.com>
2014-05-20 15:55:40 +0100

    altering cheerup to allow either an image_url or an image_file upload

--- app/models/cheerup.rb
+++ app/models/cheerup.rb
@@ -1,5 +1,5 @@
 class Cheerup < ActiveRecord::Base
-  attr_accessible :user_id, :image_url, :message, :state
+  attr_accessible :user_id, :image_url, :message, :state, :image_file
 
   belongs_to :user
   has_many :feedbacks
@@ -7,18 +7,17 @@ class Cheerup < ActiveRecord::Base
   validates :message, presence: true
   validates_length_of :message, maximum: 141 
   validates :user_id, presence: true
-  validates :image_url, presence: true
+  validate  :only_one_of_image_url_and_image_file
   validates :state, :inclusion => {:in => ["flagged", "banned", "published"]}
 
   scope :bans, -> { where(state: "banned") }
   scope :flags, -> { where(state: "flagged") }
   scope :published, -> { where(state: "published") }
   
-  #mount_uploader :image_url, CheerupImageUploader
-
-  #mount_uploader :image_file, CheerupImageUploader
+  mount_uploader :image_file, CheerupImageUploader
 
   after_initialize :defaults
+  before_validation :clear_image_file_if_image_url_given
 
   def likes_count
     self.feedbacks.where(kind: "like").count
@@ -30,6 +29,23 @@ class Cheerup < ActiveRecord::Base
 
   def defaults
     self.image_url ||= 'none'
+  end
+
+  def calculated_image_url
+    image_url.present? ? image_url : image_file_url
+  end
+
+  private
+  def only_one_of_image_url_and_image_file
+    errors.add :base, "you can only include an image or an image url" if image_file_url.present? && image_url.present? 
+  end
+
+  private
+  def clear_image_file_if_image_url_given
+    # if the image_url has been updated, but the image_file hasn't, assume the user wanted to clear the image_file
+    if image_url_changed? && !image_file_changed?
+      self.remove_image_file!
+    end
   end
 
 end
