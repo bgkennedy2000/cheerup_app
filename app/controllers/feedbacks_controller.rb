37b7a058841d423c1da6e351bcf3a88eafea9344
louiechristie <post@louiechristie.com>
2014-05-20 15:24:49 +0100

    rating, bugfix, only one rating going into database

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -4,13 +4,13 @@ class FeedbacksController < ApplicationController
 
     @user = current_user
     @cheerup = Cheerup.find(params[:cheerup_id])
-    @feedback = Feedback.first_or_initialize(cheerup_id: @cheerup.id, user_id: @user.id)
-      @feedback.kind = params[:kind]
+    @feedback = Feedback.new(cheerup_id: @cheerup.id, user_id: @user.id,
+      kind: params[:kind])
 
     if @feedback.save
       redirect_to @cheerup, notice: 'Feedback was successfully created.' 
     else
-      redirect_to @cheerup, notice: 'Feedback not successfully created.' 
+      redirect_to @cheerup, notice: 'Feedback not successfully created. '
     end
 
   end
