0eea920916784e568c1ff1f44e6b69128e031fa0
louiechristie <post@louiechristie.com>
2014-05-21 14:31:18 +0100

    homepage, featured cheerup with arrowsworking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
 
   def index
-    @new_cheerups = Cheerup.order('id').limit(9).reverse
+    @page = params.fetch(:page, 1).to_i
+    per_page = 1
+    @amount_of_pages = (Cheerup.count.to_f / per_page).ceil
 
-    @cheerup = @new_cheerups.first
+    @new_cheerups = Cheerup.offset(@page*per_page).limit(per_page).order(:created_at)
+    @new_cheerups.reverse!
+    @cheerup = @new_cheerups.last
 
     @best_cheerups = Cheerup.all
     @best_cheerups.sort!{ |a,b| a.rating <=> b.rating }.reverse!
@@ -12,6 +16,7 @@ class HomeController < ApplicationController
     @best_cheerupers = User.all
     @best_cheerupers.sort!{ |a,b| a.rating <=> b.rating }.reverse!
     @best_cheerupers = @best_cheerupers[0..1]
+
   end
 
 end
