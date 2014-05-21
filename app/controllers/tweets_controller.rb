f541163446bf3bd85ec18af59678899c041bcfd2
louiechristie <post@louiechristie.com>
2014-05-21 15:31:33 +0100

    tweet cheerups by other users

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
 
   def create
     @cheerup = Cheerup.find(params[:tweet][:cheerup_id].to_i)
-    @cheerup.tweet
+    current_user.tweet(@cheerup)
     redirect_to @cheerup, notice: 'Tweet sent.' 
   end
 
